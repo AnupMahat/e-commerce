f6cde19ee30b45e2870273bf69dde5bcf1b989d4
AnupMahat <mahatanup8@gmail.com>
2021-02-09 13:56:22 +1100

    finishing sign in hamdleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 import './sign-in.styles.scss'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends Component{
     constructor(props){
@@ -12,9 +12,16 @@ class SignIn extends Component{
             password:''
         }
     }
-    handleSubmit=(e)=>{
+    handleSubmit=async (e)=>{
         e.preventDefault()
-        this.setState({email:'',password:''})
+        const {email,password} = this.state
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email:'',password:''})
+        }catch(error){
+            console.log(error)
+        }
+        
     }
     handleChange=(e)=>{
         const{value,name} = e.target
